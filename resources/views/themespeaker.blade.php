a023257980241194a3fbb5fe15e710d98e5f6222
Dan Hermann <Dh78@me.com>
2022-08-07 19:34:33 -0500

    Float end

--- resources/views/themespeaker.blade.php
+++ resources/views/themespeaker.blade.php
@@ -16,13 +16,14 @@
     <div class="container px-3 py-5 px-lg-10 py-lg-6 bg-grey mb-5">
         <x-layouts.blog title="Theme Speaker">
             <p>
-                <img class="float-right p-3" src="/images/mishasm.png"
+                <img class="float-end p-3" src="/images/mishasm.png"
                      alt="Rev. Misha Sanders" data-no-retina/>
                 <strong>Rev. Misha Sanders</strong> (she/her/hers) is feeling very blessed to be the Senior Minister at
                 Northwest Unitarian Universalist Congregation in beautiful Sandy Springs, Georgia. She is a fiery
                 preacher of the good news of Unitarian Universalism, and believes that the whole world is built and
                 rebuilt by the stories we tell ourselves and each other.
             </p>
+            <div class="clearfix"></div>
         </x-layouts.blog>
     </div>
 @endsection
