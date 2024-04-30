1eb25fb22aecd392936be4d1d8ab18a0cc728b34
Dhermann27 <dan@cbwcw.co>
2024-04-30 08:19:44 -0500

    Amazon SES

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'ses'),
 
     /*
     |--------------------------------------------------------------------------
@@ -46,12 +46,6 @@ return [
 
         'ses' => [
             'transport' => 'ses',
-            'host' => env('MAIL_HOST', 'amazonses.com'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
         ],
 
         'mailgun' => [
