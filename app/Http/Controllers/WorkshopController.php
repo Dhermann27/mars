41257ca8fcc3df709274d58413443d24e685519f
Dhermann27 <dan@cbwcw.co>
2024-05-11 15:04:50 -0500

    whereWithHas single argument

--- app/Http/Controllers/WorkshopController.php
+++ app/Http/Controllers/WorkshopController.php
@@ -68,9 +68,9 @@ class WorkshopController extends Controller
                 $request->session()->flash('warning', 'There are no campers registered for this year.');
             }
         }
-        $timeslots = Timeslot::withWhereHas(['workshops' => function ($query) {
+        $timeslots = Timeslot::withWhereHas('workshops', function ($query) {
             $query->where('year_id', $this->year->id);
-        }])->get();
+        })->get();
         return view('workshopchoice', ['timeslots' => $timeslots, 'campers' => $campers,
             'stepdata' => $steps]);
 
