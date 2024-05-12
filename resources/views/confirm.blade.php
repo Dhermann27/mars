64a1f70af26aaf8e3921d875ab2e0e7b687f4f71
Dhermann27 <dan@cbwcw.co>
2024-05-12 14:35:33 -0500

    Section EOF, why is this loading?

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -128,119 +128,119 @@
 
         <footer style="text-align: center;"><h4>See you next week!</h4></footer>
 
-{{--        @if(count($families) == 1)--}}
-{{--            <div class="accordion" id="accordion-medicalResponses">--}}
-{{--                @foreach($family->campers()->where('age', '<', '18')->get() as $camper)--}}
-{{--                    @component('components.accordioncard', ['id' => $camper->id, 'show' => $loop->first,--}}
-{{--                        'heading' => $camper->firstname . ' ' . $camper->lastname, 'parent' => 'medicalResponses'])--}}
-{{--                        @if(isset($camper->medicalresponse))--}}
-{{--                            <div class="alert alert-success float-right m-3">--}}
-{{--                                <i class="fas fa-check" title="Medical Response Submitted"></i> Submitted!--}}
-{{--                            </div>--}}
-{{--                        @endif--}}
-{{--                        --}}{{--                        @if(count($families) == 1 && !empty($camper->program->letter))--}}
-{{--                        --}}{{--                            {!! $camper->program->letter !!}--}}
-{{--                        --}}{{--                        @endif--}}
-{{--                        <form class="form-horizontal medicalresponse" role="form" method="POST"--}}
-{{--                              action="{{ route('confirm.store') }}">--}}
-{{--                            @include('includes.flash')--}}
+        {{--        @if(count($families) == 1)--}}
+        {{--            <div class="accordion" id="accordion-medicalResponses">--}}
+        {{--                @foreach($family->campers()->where('age', '<', '18')->get() as $camper)--}}
+        {{--                    @component('components.accordioncard', ['id' => $camper->id, 'show' => $loop->first,--}}
+        {{--                        'heading' => $camper->firstname . ' ' . $camper->lastname, 'parent' => 'medicalResponses'])--}}
+        {{--                        @if(isset($camper->medicalresponse))--}}
+        {{--                            <div class="alert alert-success float-right m-3">--}}
+        {{--                                <i class="fas fa-check" title="Medical Response Submitted"></i> Submitted!--}}
+        {{--                            </div>--}}
+        {{--                        @endif--}}
+        {{--                        --}}{{--                        @if(count($families) == 1 && !empty($camper->program->letter))--}}
+        {{--                        --}}{{--                            {!! $camper->program->letter !!}--}}
+        {{--                        --}}{{--                        @endif--}}
+        {{--                        <form class="form-horizontal medicalresponse" role="form" method="POST"--}}
+        {{--                              action="{{ route('confirm.store') }}">--}}
+        {{--                            @include('includes.flash')--}}
 
-{{--                            @include('components.medical', ['camper' => $camper, 'first' => $loop->first])--}}
+        {{--                            @include('components.medical', ['camper' => $camper, 'first' => $loop->first])--}}
 
-{{--                            @if(!isset($readonly) || $readonly === false)--}}
-{{--                                <div class="form-group row d-print-none">--}}
-{{--                                    <label for="submit" class="col-md-4 control-label">&nbsp;</label>--}}
-{{--                                    <div class="col-md-6">--}}
-{{--                                        <div class="text-lg-right">--}}
-{{--                                            @if($camper->medicalresponse)--}}
-{{--                                                <button class="btn btn-lg btn-success submit py-3 px-4"><i--}}
-{{--                                                        class="fas fa-check"></i> Saved--}}
-{{--                                                </button>--}}
-{{--                                            @else--}}
-{{--                                                <button class="btn btn-lg btn-primary submit py-3 px-4">Save--}}
-{{--                                                    Response--}}
-{{--                                                </button>--}}
-{{--                                            @endif--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            @endif--}}
-{{--                        </form>--}}
-{{--                    @endcomponent--}}
-{{--                @endforeach--}}
-{{--                <footer>Please print this page using your browser's print (Ctrl+P or &#8984+P) function. Regardless--}}
-{{--                    of appearance on your screen, forms will be paginated correctly.--}}
-{{--                </footer>--}}
-{{--                @endif--}}
-{{--                @endforeach--}}
-{{--                @endsection--}}
+        {{--                            @if(!isset($readonly) || $readonly === false)--}}
+        {{--                                <div class="form-group row d-print-none">--}}
+        {{--                                    <label for="submit" class="col-md-4 control-label">&nbsp;</label>--}}
+        {{--                                    <div class="col-md-6">--}}
+        {{--                                        <div class="text-lg-right">--}}
+        {{--                                            @if($camper->medicalresponse)--}}
+        {{--                                                <button class="btn btn-lg btn-success submit py-3 px-4"><i--}}
+        {{--                                                        class="fas fa-check"></i> Saved--}}
+        {{--                                                </button>--}}
+        {{--                                            @else--}}
+        {{--                                                <button class="btn btn-lg btn-primary submit py-3 px-4">Save--}}
+        {{--                                                    Response--}}
+        {{--                                                </button>--}}
+        {{--                                            @endif--}}
+        {{--                                        </div>--}}
+        {{--                                    </div>--}}
+        {{--                                </div>--}}
+        {{--                            @endif--}}
+        {{--                        </form>--}}
+        {{--                    @endcomponent--}}
+        {{--                @endforeach--}}
+        {{--                <footer>Please print this page using your browser's print (Ctrl+P or &#8984+P) function. Regardless--}}
+        {{--                    of appearance on your screen, forms will be paginated correctly.--}}
+        {{--                </footer>--}}
+        {{--                @endif--}}
+        {{--                @endforeach--}}
+        {{--                @endsection--}}
 
-{{--                @section('script')--}}
-{{--                    <script>--}}
-{{--                        $("button.copyanswers").on('click', function (e) {--}}
-{{--                            e.preventDefault();--}}
-{{--                            var myform = $(this).parents("form");--}}
-{{--                            var first = $("form.medicalresponse").first();--}}
-{{--                            var elements = myform.find("input, textarea, select");--}}
-{{--                            if (first.find('select[name*="is_insured"]').val() === '1') {--}}
-{{--                                myform.find("div.insurance").removeClass("d-none");--}}
-{{--                            }--}}
-{{--                            first.find("input, textarea, select").each(function (index) {--}}
-{{--                                if ($(this).attr("type") !== "checkbox") {--}}
-{{--                                    elements[index].value = $(this).val();--}}
-{{--                                } else {--}}
-{{--                                    elements[index].checked = $(this).checked;--}}
-{{--                                }--}}
-{{--                            });--}}
-{{--                            return false;--}}
-{{--                        });--}}
+        {{--                @section('script')--}}
+        {{--                    <script>--}}
+        {{--                        $("button.copyanswers").on('click', function (e) {--}}
+        {{--                            e.preventDefault();--}}
+        {{--                            var myform = $(this).parents("form");--}}
+        {{--                            var first = $("form.medicalresponse").first();--}}
+        {{--                            var elements = myform.find("input, textarea, select");--}}
+        {{--                            if (first.find('select[name*="is_insured"]').val() === '1') {--}}
+        {{--                                myform.find("div.insurance").removeClass("d-none");--}}
+        {{--                            }--}}
+        {{--                            first.find("input, textarea, select").each(function (index) {--}}
+        {{--                                if ($(this).attr("type") !== "checkbox") {--}}
+        {{--                                    elements[index].value = $(this).val();--}}
+        {{--                                } else {--}}
+        {{--                                    elements[index].checked = $(this).checked;--}}
+        {{--                                }--}}
+        {{--                            });--}}
+        {{--                            return false;--}}
+        {{--                        });--}}
 
-{{--                        $('form.medicalresponse select[name*="is_insured"]').on('change', function (e) {--}}
-{{--                            if ($(this).val() === '1') {--}}
-{{--                                $(this).parent().parent().next().removeClass("d-none");--}}
-{{--                            } else {--}}
-{{--                                $(this).parent().parent().next().addClass("d-none");--}}
-{{--                            }--}}
-{{--                        });--}}
+        {{--                        $('form.medicalresponse select[name*="is_insured"]').on('change', function (e) {--}}
+        {{--                            if ($(this).val() === '1') {--}}
+        {{--                                $(this).parent().parent().next().removeClass("d-none");--}}
+        {{--                            } else {--}}
+        {{--                                $(this).parent().parent().next().addClass("d-none");--}}
+        {{--                            }--}}
+        {{--                        });--}}
 
 
-{{--                        $('form.medicalresponse button.submit').on('click', function (e) {--}}
-{{--                            e.preventDefault();--}}
-{{--                            var form = $(this).parents("form");--}}
-{{--                            $(this).val("Saving").removeClass("btn-primary btn-danger").prop("disabled", true);--}}
-{{--                            form.find(".has-danger").removeClass("has-danger");--}}
-{{--                            form.find(".is-invalid").removeClass("is-invalid");--}}
-{{--                            form.find(".invalid-feedback").remove();--}}
-{{--                            $("div.alert").remove();--}}
-{{--                            $.ajax({--}}
-{{--                                url: form.attr("action"),--}}
-{{--                                type: 'post',--}}
-{{--                                data: form.serialize(),--}}
-{{--                                async: false,--}}
-{{--                                success: function (data) {--}}
-{{--                                    form.before("<div class='alert alert-success'>" + data + "</div>");--}}
-{{--                                    form.find("button").text("Saved").addClass("btn-success").prop("disabled", false);--}}
-{{--                                    if (form.parents(".card").next(".card") !== undefined) {--}}
-{{--                                        form.parents(".card").next(".card").find(".collapse").collapse('show');--}}
-{{--                                    }--}}
-{{--                                },--}}
-{{--                                error: function (data) {--}}
-{{--                                    if (data.status === 500) {--}}
-{{--                                        form.before("<div class='alert alert-danger'>Unknown error occurred. Please use the Contact Us form to ask for assistance and include the approximate time you received this message.</div>");--}}
-{{--                                    } else {--}}
-{{--                                        var errorCount = data !== undefined ? Object.keys(data.responseJSON.errors).length : '';--}}
-{{--                                        $.each(data.responseJSON.errors, function (k, v) {--}}
-{{--                                            var group = $("#" + k).parents(".form-group").addClass("has-danger");--}}
-{{--                                            group.find("select,input").addClass('is-invalid');--}}
-{{--                                            group.find("div:first").append("<span class=\"invalid-feedback\"><strong>" + this[0] + "</strong></span>");--}}
-{{--                                        });--}}
-{{--                                        $("span.invalid-feedback").show();--}}
-{{--                                        form.before("<div class='alert alert-danger'>You have " + errorCount + " error(s) in your form. Please adjust your entries and resubmit.</div>");--}}
-{{--                                    }--}}
-{{--                                    form.find("button").text("Resubmit").addClass("btn-danger").prop("disabled", false);--}}
-{{--                                }--}}
-{{--                            });--}}
-{{--                        });--}}
-{{--                    </script>--}}
-{{--            @endsection--}}
+        {{--                        $('form.medicalresponse button.submit').on('click', function (e) {--}}
+        {{--                            e.preventDefault();--}}
+        {{--                            var form = $(this).parents("form");--}}
+        {{--                            $(this).val("Saving").removeClass("btn-primary btn-danger").prop("disabled", true);--}}
+        {{--                            form.find(".has-danger").removeClass("has-danger");--}}
+        {{--                            form.find(".is-invalid").removeClass("is-invalid");--}}
+        {{--                            form.find(".invalid-feedback").remove();--}}
+        {{--                            $("div.alert").remove();--}}
+        {{--                            $.ajax({--}}
+        {{--                                url: form.attr("action"),--}}
+        {{--                                type: 'post',--}}
+        {{--                                data: form.serialize(),--}}
+        {{--                                async: false,--}}
+        {{--                                success: function (data) {--}}
+        {{--                                    form.before("<div class='alert alert-success'>" + data + "</div>");--}}
+        {{--                                    form.find("button").text("Saved").addClass("btn-success").prop("disabled", false);--}}
+        {{--                                    if (form.parents(".card").next(".card") !== undefined) {--}}
+        {{--                                        form.parents(".card").next(".card").find(".collapse").collapse('show');--}}
+        {{--                                    }--}}
+        {{--                                },--}}
+        {{--                                error: function (data) {--}}
+        {{--                                    if (data.status === 500) {--}}
+        {{--                                        form.before("<div class='alert alert-danger'>Unknown error occurred. Please use the Contact Us form to ask for assistance and include the approximate time you received this message.</div>");--}}
+        {{--                                    } else {--}}
+        {{--                                        var errorCount = data !== undefined ? Object.keys(data.responseJSON.errors).length : '';--}}
+        {{--                                        $.each(data.responseJSON.errors, function (k, v) {--}}
+        {{--                                            var group = $("#" + k).parents(".form-group").addClass("has-danger");--}}
+        {{--                                            group.find("select,input").addClass('is-invalid');--}}
+        {{--                                            group.find("div:first").append("<span class=\"invalid-feedback\"><strong>" + this[0] + "</strong></span>");--}}
+        {{--                                        });--}}
+        {{--                                        $("span.invalid-feedback").show();--}}
+        {{--                                        form.before("<div class='alert alert-danger'>You have " + errorCount + " error(s) in your form. Please adjust your entries and resubmit.</div>");--}}
+        {{--                                    }--}}
+        {{--                                    form.find("button").text("Resubmit").addClass("btn-danger").prop("disabled", false);--}}
+        {{--                                }--}}
+        {{--                            });--}}
+        {{--                        });--}}
+        {{--                    </script>--}}
+        @endsection
 
